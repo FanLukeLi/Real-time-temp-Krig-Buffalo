d3b9f14d70d7086ef380e39b0e974f4190605fd5
Fan Luke Li <lfgeology@outlook.com>
2025-03-27 22:43:46 +0800

    building main app

--- kriging_app/app.py
+++ kriging_app/app.py
@@ -1,9 +1,33 @@
 from flask import Flask
+import time
+from collections import deque
+from datetime import datetime
+from threading import Thread
 from extract_data import main as extract_data
 from kriging_app import main as kriging_interp
+from visualize import main as visualize
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello(): 
+    queue = deque([datetime.now().hour])
+
+    def maintain_queue(): 
+        queue.append(queue[-1] + 1)
+
+    
+    def make_html(): 
+        extract_data()
+        res = kriging_interp()
+        visualize(res['coord_x'], res['coord_y'], res['temperature'])
+        # Reload html
+
+
+    while queue: 
+        maintain_queue()
+        Thread(make_html).start()
+        time.sleep(3600)
+
     return "Hello, World!"
