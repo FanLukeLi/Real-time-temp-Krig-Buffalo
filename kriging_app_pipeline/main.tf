c1cf2f01059db16e3d6b1fcceef33ebdab0a9baf
Fan Li <43713448+FanLukeLi@users.noreply.github.com>
2025-04-20 03:50:43 +0000

    updated maint.tf

--- kriging_app_pipeline/main.tf
+++ kriging_app_pipeline/main.tf
@@ -1,13 +1,16 @@
-resource "docker_image" "nginx" {
-    name = "nginx:latest"
-    keep_locally = false
+provider "azurerm" {
+    features {}
 }
 
-resource "docker_container" "nginx" {
-    image = docker_image.nginx.latest
-    name = "webserver"
-    ports {
-        internal = 80
-        external = 8000
-    }
+resource "azurerm_resource_group" "rg" {
+    name = "azure_devops_learning"
+    location = "East US"
+}
+
+resource "azurerm_service_plan" "appserviceplan" {
+    name                = "myAppServicePlan"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    os_type             = "Linux"
+    sku_name            = "B1"
 }
