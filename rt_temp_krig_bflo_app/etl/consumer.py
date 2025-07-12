e1a77a6c53baa6a761311731cbd4c123cb48b69e
Fan Luke Li <lfgeology@outlook.com>
2025-07-12 15:26:35 +0800

    Debugged to reach a stable version of the app for writing data to and reading spark dataframe from kafka

--- rt_temp_krig_bflo_app/etl/consumer.py
+++ rt_temp_krig_bflo_app/etl/consumer.py
@@ -5,33 +5,41 @@ import sys
 import logging
 import pandas as pd
 from pyspark.sql import SparkSession
+from kafka import KafkaConsumer
 
 
-def main(): 
+def main():
     logging.info("START")
     spark = SparkSession.builder \
         .appName("rt-krig-map") \
         .master("spark://spark:7077") \
-        .config("spark.executor.memory", "1g") \
-        .config("spark.driver.memory", "512m") \
-        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0") \
+        .config("spark.driver.port", "4041") \
+        .config("spark.blockManager.port", "4042") \
+        .config("spark.driver.bindAddress", "0.0.0.0") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0") \
         .getOrCreate()
+        # .config("spark.executor.memory", "4g") \
+        # .config("spark.driver.memory", "4g") \
 
-    data_df = spark.read.format("kafka") \
-        .option("kafka.bootstrap.servers", "kafka:9092") \
-        .option("subscribe", "temp_data_bflo") \
-        .load() \
-        .selectExpr("CAST(value AS STRING) as raw_data", "CAST(key AS STRING) as batch_id")
+    consumer = KafkaConsumer(
+        'batch_end_mark',
+        bootstrap_servers='kafka:9092',
+        auto_offset_reset='latest',
+        enable_auto_commit=True,
+        group_id='rt_temp_krig_bflo_group',
+        session_timeout_ms=30000,
+    )
 
-    grouped_df = data_df.groupBy("batch_id").agg({"raw_data": "collect_list"})
+    for message in consumer:
+        id_prefix = message.value
+        data_df = spark.read.format("kafka") \
+            .option("kafka.bootstrap.servers", "kafka:9092") \
+            .option("subscribe", "temp_data_bflo") \
+            .load() \
+            .selectExpr("CAST(value AS STRING) as raw_data", "CAST(key AS STRING) as batch_id") \
+            .filter(f"batch_id = '{id_prefix}'")
 
-    # grouped_df.writeStream.format("console").outputMode("append").start()
-
-    pd_df = grouped_df.toPandas()
-
-    pd_df[["temperature", "longitude", "latitude"]] = pd_df["raw_data"].apply(lambda x: pd.Series(x.split(","))).astype(float)
-
-    pd_df.to_string(sys.stdout)
+        print(data_df)
 
 
 if __name__ == '__main__':
--- rt_temp_krig_bflo_app/etl/producer.py
+++ rt_temp_krig_bflo_app/etl/producer.py
@@ -6,10 +6,42 @@ import requests
 import datetime
 import geopandas as gpd
 from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
 from apscheduler.schedulers.background import BackgroundScheduler
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
 
 with open('./config.json', 'r') as f:
     config = json.load(f)
+
+def create_topics():
+    admin_client = KafkaAdminClient(
+        bootstrap_servers='kafka:9092',
+        client_id='producer-admin'
+    )
+
+    topics = [
+        NewTopic(name='temp_data_bflo', num_partitions=1, replication_factor=1),
+        NewTopic(name='batch_end_mark', num_partitions=1, replication_factor=1)
+    ]
+
+    try:
+        # existing_topics = admin_client.list_topics()
+        # topics_to_delete = [topic for topic in ['temp_data_bflo', 'batch_end_mark'] if topic in existing_topics]
+        # if topics_to_delete:
+        #     admin_client.delete_topics(topics_to_delete)
+        #     logging.info(f"Deleted topics: {topics_to_delete}")
+        # time.sleep(5)
+        admin_client.create_topics(new_topics=topics, validate_only=False)
+        logging.info("Topics created successfully. ")
+    except Exception as e:
+        logging.warning(f"Error creating topics: {e}")
+    finally:
+        admin_client.close()
 
 def get_temp(x, y): 
     response = requests.get("https://api.open-meteo.com/v1/forecast", params={
@@ -22,6 +54,8 @@ def get_temp(x, y):
 
 
 def main():
+    # create_topics()
+
     buf_coords = gpd.read_file('./data/temp_request_grid.json')
     producer = KafkaProducer(
         bootstrap_servers='kafka:9092',
@@ -32,16 +66,16 @@ def main():
         id_prefix = datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")
         for i, coord in buf_coords.iterrows():
             x = coord.geometry.x
-            y = coord.geometry.x
+            y = coord.geometry.y
             try:
                 res = get_temp(x, y)
                 temp = res['current']['temperature_2m']
             except Exception as e:
                 logging.info(str(e))
                 temp = float('inf')
-            msg = {id_prefix: f"{temp},{x},{y}"}
-            print(msg)
-            producer.send('temp_data_bflo', msg)
+            logging.info(f"Batch id {id_prefix}: {temp}, {x}, {y}")
+            producer.send('temp_data_bflo', f"{temp},{x},{y}")
+        producer.send('batch_end_mark', id_prefix)
 
     scheduler = BackgroundScheduler()
     scheduler.add_job(send_msgs, "interval", seconds=config['interval'])
@@ -59,5 +93,5 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.info("START")
+    logging.info("START", )
     main()
